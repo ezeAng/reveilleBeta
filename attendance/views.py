3a795836fcdfacd6d07dde20ea8d4bd13052c8c5
Wong Wei Bin <wongweibin99@gmail.com>
2020-07-02 21:50:55 +0800

    added context for summary view

--- attendance/views.py
+++ attendance/views.py
@@ -44,28 +44,62 @@ def parade_view(request):
 		else:
 			parade = parade.values()[0]
 			logger.info('PARADE OBJ %s',parade)
+
 			parade_id = parade['id']
+			total_strength = parade['total_strength']
+			current_strength = parade['commander_strength'] + parade['personnel_strength']
 			absentees = Absence.objects.filter(
 				parade_id = parade_id
-			)
-
-			if len(absence) == 0:
-				raise Exception('No absentees recorded')
-
+			).values()
+			logger.info('ABSENCE %s', absentees)
+			all_present = False
+			if len(absentees) == 0:
+				all_present = True
+			
 			else:
-				context = {
-					'parade': parade,
-					'absentees': absentees
-				}
-				return render(request, 'attendance/revhome.html/', context)
+				total_attc = 0
+				total_ma = 0
+				total_leave = 0
+				total_off = 0
+				total_other = 0
+				
+				for abs in absentees:
+					if abs['is_MC']:
+						total_attc += 1
+					elif abs['is_MA']:
+						total_ma += 1
+					elif abs['is_leave']:
+						total_leave += 1
+					elif abs['is_off']:
+						total_off += 1
+					elif abs['is_other']:
+						total_other += 1
+					else:
+						pass
+
+		context = {
+			'parade_id': parade_id,
+			'all_present': all_present,
+			'total_strength': total_strength,
+			'current_strength': current_strength,
+			'total_absent': total_strength-current_strength,
+			'total_attc': total_attc,
+			'total_ma': total_ma,
+			'total_leave': total_leave,
+			'total_off': total_off,
+			'total_other': total_other,
+		}
+
+		logger.info('RESULTS %s', context)
+		return render(request, 'attendance/revhome.html/', context)
 	
 	except Exception as identifier:
+		logging.info('ERROR %s', identifier.args[0])
 		context = {
 			'error': True,
 			'message': identifier.args[0]
 		}
 		return render(request, 'attendance/revhome.html/', context)
-
 
 
 def dashboard_view(request):
