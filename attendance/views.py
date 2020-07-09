97880156d6e4285d75ff922ab70f65803a162d8d
Wong Wei Bin <wongweibin99@gmail.com>
2020-07-10 00:23:13 +0800

    kukubirrdd

--- attendance/views.py
+++ attendance/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.generic import View
 from .models import Personnel, Absence, Parade
 import datetime
 import logging
 import sys
+from django.db import transaction
+from reveille.utils import ParadeStrengthCalculator
 
 def home_view(request):
 	# parade_id = request.GET.get('parade_id')
@@ -19,27 +22,47 @@ def home_view(request):
 	# 	}
 	context = {'default': True}
 	return render(request, 'attendance/revhome.html/', context)
-		
+
+# class ParadeView(View):
+# 	logger = logging.getLogger(__name__)
+# 	date = request.GET.get('date')
+# 	formatted_date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
+# 	logger.info('DATE %s',formatted_date)
+
+# 	time_of_day = int(request.GET.get('time_of_day'))
+# 	logger.info('TIME OF DAY %s',time_of_day)
+
+# 	def get(self, request, *args, **kwargs):
+# 		pass
+# 	def post(self, request, *args, **kwargs):
+# 		pass
+
 def parade_view(request):
 	logger = logging.getLogger(__name__)
 	date = request.GET.get('date')
-	date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
-	logger.info('DATE %s',date)
+	formatted_date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
+	logger.info('DATE %s',formatted_date)
 
 	time_of_day = int(request.GET.get('time_of_day'))
 	logger.info('TIME OF DAY %s',time_of_day)
+	transaction.set_autocommit(False)
+
 	
 	try:
 		parade = Parade.objects.filter(
-			date = date, time_of_day = time_of_day
+			date = formatted_date, time_of_day = time_of_day
 		)
-		logger.info(parade)
+		logger.info('PARADE %s', parade)
+		card_data = []
+		parade_summary = None
 
 		# Parade does not exist
 		if len(parade) == 0:
+			logger.info('Lanjiao')
 			parade_exist = False
 
 		else:
+			logger.info('jibai')
 			parade_exist = True
 			parade = parade.values()[0]
 			logger.info('PARADE OBJ %s',parade)
@@ -54,10 +77,10 @@ def parade_view(request):
 			logger.info('ABSENCE %s', absentees)
 			
 			no_absentees = True
+
 			if len(absentees) == 0:
 				no_absentees = False
-				card_data = []
-			
+				
 			else:
 				total_attc = 0
 				total_ma = 0
@@ -65,7 +88,6 @@ def parade_view(request):
 				total_off = 0
 				total_other = 0
 				
-				card_data = []
 				for abs in absentees:
 					if abs['is_MC']:
 						total_attc += 1
@@ -97,20 +119,76 @@ def parade_view(request):
 					card_data.append(data)
 					logger.info('CARD DATA %s', card_data )
 
-		parade_summary = {
-			'parade_id': parade_id,
-			'no_absentees': no_absentees,
-			'total_strength': total_strength,
-			'current_strength': current_strength,
-			'total_absent': total_strength-current_strength,
-			'total_attc': total_attc,
-			'total_ma': total_ma,
-			'total_leave': total_leave,
-			'total_off': total_off,
-			'total_other': total_other,
-		}
+			parade_summary = {
+				'parade_id': parade_id,
+				'total_strength': total_strength,
+				'current_strength': current_strength,
+				
+				# take from utils, do .update
+				'no_absentees': no_absentees,
+				'total_absent': total_strength-current_strength,
+				'total_attc': total_attc,
+				'total_ma': total_ma,
+				'total_leave': total_leave,
+				'total_off': total_off,
+				'total_other': total_other,
+			}
 
-		if request.method == 'GET':
+		
+		if request.method == 'POST':
+			logger.info('POST DATA %s', request.POST)
+			name = request.POST.get('Name')
+			remarks = request.POST.get('Remarks')
+			absence = request.POST.get('Absence')
+			transaction.set_autocommit(False)
+
+			try:
+				if parade_exist:
+					pass
+
+				else:
+					parade = Parade(
+						date = formatted_date, 
+						time_of_day = time_of_day
+					)
+					parade.save()
+					parade_id = parade.id
+					parade_instance = ParadeStrengthCalculator(parade_id)
+					parade_instance.update_parade_instance()
+
+				personnel_obj = Personnel.objects.get(
+					name = name
+				)
+				absence = Absence(
+					personnel = personnel_obj,
+					parade_id = parade_id,
+					remarks = remarks,
+				)
+				if absence == 'MA':
+					absence.is_MA = True
+				elif absence == 'MC':
+					absence.is_MC = True
+				elif absence == 'Off':
+					absence.is_off = True
+				elif absence == 'Leave':
+					absence.is_leave = True
+				elif absence == 'Others':
+					absence.is_other = True
+				else:
+					pass
+				absence.save()
+			
+			except Exception as identifier:
+				transaction.rollback()
+				raise Exception(identifier.args[0])
+			transaction.commit()
+
+			logger.info('GET DATE %s', date)
+			logger.info('GET TOD, %s', str(time_of_day))
+			return HttpResponseRedirect(
+				request.path_info + '?date=' + date + '&time_of_day=' + str(time_of_day))
+		
+		elif request.method == 'GET':
 			context = {
 				'parade_exist': parade_exist,
 				'parade_summary': parade_summary,
@@ -120,25 +198,8 @@ def parade_view(request):
 			logger.info('RESULTS %s', context)
 			return render(request, 'attendance/revhome.html/', context)
 		
-		if request.method == 'POST':
-			logger.info('POST DATA %s', request.POST)
-			if parade_exist:
-					
-			
-			else:
-				# create new parade if it does not exist
-				parade = Parade(
-
-				)
-				parade.save()
-				return parade
-			# '''
-			absence = Absence(
-				personnel = personnel_object,
-				parade = 
-
-			)
-			# '''
+		else:
+			raise Exception('Method not allowed')
 	
 	except Exception as identifier:
 		logging.info('ERROR %s', identifier.args[0])
@@ -147,6 +208,7 @@ def parade_view(request):
 			'message': 'Hong gan liao unexpected error occured. Please contact your encik grandmother for support.'
 		}
 		return render(request, 'attendance/revhome.html/', context)
+
 
 # def add_card_view(request):
 	
--- reveille/utils.py
+++ reveille/utils.py
@@ -4,33 +4,106 @@ from attendance.models import (
     Status, 
     Parade
 )
+import logging
+import sys
+
+class ParadeStrengthCalculator:
+    logger = logging.getLogger(__name__)
+    def __init__(self, parade_id):
+        parade = Parade.objects.get(id = parade_id)
+        self.parade = parade
+        self.parade_id = parade_id
+
+    def calc_coy_total(self):
+        strength = 0
+        for i in Personnel.objects.all():
+            strength += 1
+        return strength
+
+    def calc_coy_current(self):
+        total_strength = self.calc_coy_total()
+        current_strength = total_strength
+        for i in Absence.objects.filter(id=self.parade_id):
+            current_strength -= 1
+        # return ('{}/{}'.format(current_strength,total_strength))
+        return current_strength
+    
+    def calc_comd_strength(self):
+        total_comd_strength = 0
+        for i in Personnel.objects.filter(is_commander = True):
+            total_comd_strength += 1
+        current_comd_strength = total_comd_strength
+        absent_comds = Absence.objects.filter(
+            id=self.parade_id).filter(personnel__is_commander=True)
+        for i in absent_comds:
+            current_comd_strength -= 1
+        return current_comd_strength
+
+    def calc_trpr_strength(self):
+        total_trpr_strength = 0
+        for i in Personnel.objects.filter(is_commander = False):
+            total_trpr_strength += 1
+        current_trpr_strength = total_trpr_strength
+        absent_trprs = Absence.objects.filter(
+            id=self.parade_id).filter(personnel__is_commander=False)
+        for i in absent_trprs:
+            current_trpr_strength -= 1
+        return current_trpr_strength
+
+    def calc_absence(self):
+        absentees = Absence.objects.all().filter(id=self.parade_id)
+        no_absentees = True
+        if len(absentees) == 0:
+            no_absentees = False
+        else:
+            total_attc = 0
+            total_ma = 0
+            total_leave = 0
+            total_off = 0
+            total_other = 0
+            for abs in absentees:
+                if abs['is_MC']:
+                    total_attc += 1
+                elif abs['is_MA']:
+                    total_ma += 1
+                elif abs['is_leave']:
+                    total_leave += 1
+                elif abs['is_off']:
+                    total_off += 1
+                elif abs['is_other']:
+                    total_other += 1
+                else:
+                    pass
+        data = { 
+            'no_absentees': no_absentees,
+            'total_absent': int(self.calc_coy_total) - int(self.calc_coy_current),
+            'total_attc': total_attc,
+            'total_ma': total_ma,
+            'total_leave': total_leave,
+            'total_off': total_off,
+            'total_other': total_other,
+        }
 
 
-def calc_coy_total():
-    strength = 0
-    for i in Personnel.objects.all():
-        strength += 1
-    return strength
+    def update_parade_instance(self):
+        parade = self.parade
+        parade.commander_strength = int(self.calc_comd_strength())
+        parade.personnel_strength = int(self.calc_trpr_strength())
+        parade.current_strength = int(self.calc_coy_current())
+        parade.total_strength = int(self.calc_coy_total())
+        parade.save()
+'''
+    def calc_plt_total(platoon):
+        strength = 0 
+        for i in Personnel.objects.all().filter(platoon=platoon):
+            strength += 1
 
-def calc_coy_current(parade_id):
-    total_strength = calc_coy_total()
-    current_strength = total_strength
-    for i in Absence.objects.all().filter(id=parade_id):
-        current_strength -= 1
-    return ('{}/{}'.format(current_strength,total_strength))
+    def calc_plt_current(platoon, parade_id):
+        total_strength = calc_plt_total(platoon)
+        current_strength = total_strength
+        for i in Absence.objects.all().filter(id=parade_id).filter(trooper__platoon=platoon):
+            current_strength -= 1
+        return ('{}/{}'.format(current_strength,total_strength))
+'''
 
-
-def calc_plt_total(platoon):
-    strength = 0 
-    for i in Personnel.objects.all().filter(platoon=platoon):
-        strength += 1
-
-def calc_plt_current(platoon, parade_id):
-    total_strength = calc_plt_total(platoon)
-    current_strength = total_strength
-    for i in Absence.objects.all().filter(id=parade_id).filter(trooper__platoon=platoon):
-        current_strength -= 1
-    return ('{}/{}'.format(current_strength,total_strength))
-
-
-
+    
